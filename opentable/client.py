aa7d8059910bcc30ff62b65698dbae6661c2f138
jaebradley <jae.b.bradley@gmail.com>
2017-05-24 01:35:28 -0400

    add restaurants

--- opentable/client.py
+++ opentable/client.py
@@ -1,24 +1,33 @@
 import requests
 
+from opentable.queries.query_builders import RestaurantSearchQueryParametersBuilder
+
 
 class OpenTableClient:
     base_url = "https://opentable.herokuapp.com/api"
 
     def __init__(self):
-        pass
+        self.restaurant_search_query_parameters_builder = RestaurantSearchQueryParametersBuilder()
 
-    @staticmethod
-    def get_summary_statistics():
+    def get_summary_statistics(self):
         r = requests.get(url="{base_url}/stats".format(base_url=OpenTableClient.base_url))
 
         r.raise_for_status()
 
         return r.json()
 
-    @staticmethod
-    def get_cities():
+    def get_cities(self):
         r = requests.get(url="{base_url}/cities".format(base_url=OpenTableClient.base_url))
 
         r.raise_for_status()
 
         return r.json()
+
+    def search_restaurants(self, query):
+        r = requests.get(url="{base_url}/restaurants".format(base_url=OpenTableClient.base_url),
+                         params=self.restaurant_search_query_parameters_builder.build_query_parameters(query=query))
+
+        r.raise_for_status()
+
+        return r.json()
+
---
+++ opentable/queries/query_builders.py
@@ -0,0 +1,34 @@
+class RestaurantSearchQueryParametersBuilder:
+    def __init__(self):
+        pass
+
+    def build_query_parameters(self, query):
+        query_parameters = {}
+        if query.price_range is not None:
+            query_parameters["price"] = query.price_range.value
+
+        if query.name is not None:
+            query_parameters["name"] = query.name
+
+        if query.address_line is not None:
+            query_parameters["address"] = query.address_line
+
+        if query.state_code is not None:
+            query_parameters["state"] = query.state_code.upper()
+
+        if query.city_name is not None:
+            query_parameters["city"] = query.city_name
+
+        if query.zip_code is not None:
+            query_parameters["zip"] = query.zip_code
+
+        if query.country_code is not None:
+            query_parameters["country"] = query.country_code.upper()
+
+        if query.page is not None:
+            query_parameters["page"] = query.page
+
+        if query.per_page is not None:
+            query_parameters["per_page"] = query.per_page.value
+
+        return query_parameters
